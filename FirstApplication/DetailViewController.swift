4a433e4c2b8a518d9f26ca065656ecfd88a418e0
Kevin Miyata <kjm015@yahoo.com>
2018-09-21 22:48:20 -0500

    Initial Commit

---
+++ FirstApplication/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FirstApplication
+//
+//  Created by Kevin Miyata on 9/21/18.
+//  Copyright © 2018 Kevin Miyata. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
